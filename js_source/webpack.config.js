926e3a1cfdbc62bd26e236138dbf65c9b4e3e88b
Kevin Baes <kevin.baes@student.howest.be>
2018-05-28 19:37:19 +0200

    add webpack config to create production scripts

--- js_source/webpack.config.js
+++ js_source/webpack.config.js
@@ -1,31 +1,28 @@
 const path = require('path');
 
-module.exports = {
-    mode: "development",
-    entry: {
-        index: "./js/index.js",
-        mindmap: "./js/mindmap.js"
-    },
-    devtool: "inline-source-map",
-    output: {
-        filename: "./../../backend/src/main/resources/static/assets/js/[name].js"
-    },
-    optimization: {
-        minimize: false
-      }
-};
-
 // module.exports = {
-//     mode: "production",
+//     mode: "development",
 //     entry: {
 //         index: "./js/index.js",
 //         mindmap: "./js/mindmap.js"
 //     },
+//     stats: "verbose",
+//     devtool: "source-map",
 //     output: {
 //         filename: "./../../backend/src/main/resources/static/assets/js/[name].js"
-//     }
-//     // ,
-//     // optimization: {
-//     //     minimize: false
-//     // }
+//     },
+//     optimization: {
+//         minimize: false
+//       }
 // };
+
+module.exports = {
+    mode: "production",
+    entry: {
+        index: "./js/index.js",
+        mindmap: "./js/mindmap.js"
+    },
+    output: {
+        filename: "./../../backend/src/main/resources/static/assets/js/[name].min.js"
+    }
+};
